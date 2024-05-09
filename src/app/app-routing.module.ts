ae05e80b0454af01cec9be9c191b181840357d7f
Matteo Scanferla <m.scanferla@eggonemail.com>
2024-05-09 16:39:05 +0200

    Refactor variables.scss + added registration, not started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
+import {AppComponent} from "./app.component";
+import {RegistrationComponent} from "./pages/registration/registration.component";
 
 const routes: Routes = [
   {
@@ -11,6 +13,7 @@ const routes: Routes = [
   {path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
   {
     path: 'auth',
+    component: AppComponent,
     children: [
       {
         path: '',
@@ -20,6 +23,10 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegistrationComponent
       },
     ]
   },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,12 +5,14 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import {NetworkComponent} from "../../pages/network/network.component";
 import {FormsModule} from "@angular/forms";
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
     PagesComponent,
     NetworkComponent,
+    RegistrationComponent,
   ],
   imports: [
     CommonModule,
